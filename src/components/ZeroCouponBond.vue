31f55758cafd274c716e8003dae57396fe10cb40
Craig Banach <craigsb93@gmail.com>
2018-02-12 00:16:00 +0000

    Added capital selection and result label

--- src/components/ZeroCouponBond.vue
+++ src/components/ZeroCouponBond.vue
@@ -1,63 +1,70 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col/>
-      <b-col>
-        <b-form>
-          <b-form-group id="principalLabel"
-                        label="Principal:"
-                        label-for="principalInput">
-            <b-form-input id="principalInput"
-                          v-model="form.principal"
-                          type="number"
-                          required
-                          placeholder="Enter Principal ">
-            </b-form-input>
-          </b-form-group>
-          <b-form-group id="rateLabel"
-                        label="Interest Rate:"
-                        label-for="rateInput"
-                        description="Enter Interest Rate as a decimal (i.e. 0.1)">
-            <b-form-input id="rateInput"
-                          type="number"
-                          v-model.number="form.rate"
-                          required
-                          placeholder="Enter Interest Rate">
-            </b-form-input>
-          </b-form-group>
-          <b-form-group id="termLabel"
-                        label="Investment Term:"
-                        label-for="termInput"
-                        description="Enter Investment Term in years (i.e. 4.5)">
-            <b-form-input id="termInput"
-                          type="number"
-                          v-model="form.term"
-                          required
-                          placeholder="Enter Investment Term">
-            </b-form-input>
-          </b-form-group>
-          <b-form-group id="resultLabel"
-                        v-bind:label="'Total capital after ' + form.term + ' years:'"
-                        label-for="resultText">
-            <b-form-input id="resultText"
-                          type="number"
-                          plaintext
-                          variant="info"
-                          v-bind:value="resultValue"/>
-          </b-form-group>
-          <b-form-group id="roundLabel"
-                        label="Rounding:"
-                        label-for="roundInput">
-            <b-form-radio-group id="roundInput"
-                                v-model="roundingSelection"
-                                :options="roundingOptions">
-            </b-form-radio-group>
-          </b-form-group>
-        </b-form>
-      </b-col>
-      <b-col/>
-    </b-row>
-  </b-container>
+  <b-row>
+    <b-col/>
+    <b-col>
+      <b-form>
+        <b-input-group v-bind:prepend="CapitalSelection">
+          <b-form-input id="principalInput"
+                        v-model="form.principal"
+                        type="number"
+                        required>
+          </b-form-input>
+          <template slot="append">
+            <b-dropdown text="Select">
+              <b-dropdown-item id="capitalDropdown"
+                               v-on:click="CapitalSelection = 'Initial Capital'">
+                Initial Capital
+              </b-dropdown-item>
+              <b-dropdown-item id="returnDropdown"
+                               v-on:click="CapitalSelection = 'Bond Value'">
+                Bond Value
+              </b-dropdown-item>
+            </b-dropdown>
+          </template>
+        </b-input-group>
+        <b-form-group id="rateLabel"
+                      label="Interest Rate:"
+                      label-for="rateInput"
+                      description="Enter Interest Rate as a decimal (i.e. 0.1)">
+          <b-form-input id="rateInput"
+                        type="number"
+                        v-model.number="form.rate"
+                        required
+                        placeholder="Enter Interest Rate">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="termLabel"
+                      label="Investment Term:"
+                      label-for="termInput"
+                      description="Enter Investment Term in years (i.e. 4.5)">
+          <b-form-input id="termInput"
+                        type="number"
+                        v-model="form.term"
+                        required
+                        placeholder="Enter Investment Term">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="resultLabel"
+                      v-bind:label="ResultLabel"
+                      label-for="resultText">
+          <b-form-input id="resultText"
+                        type="number"
+                        plaintext
+                        variant="info"
+                        v-bind:value="resultValue"/>
+        </b-form-group>
+        <b-form-group id="roundLabel"
+                      label="Rounding:"
+                      label-for="roundInput">
+          <b-form-radio-group id="roundInput"
+                              v-model="roundingSelection"
+                              :options="roundingOptions">
+          </b-form-radio-group>
+        </b-form-group>
+      </b-form>
+    </b-col>
+    <b-col/>
+  </b-row>
 </template>
 
 <script>
@@ -72,17 +79,46 @@ export default {
       form: {
         principal: 100,
         rate: 0.05,
-        term: 10
+        term: 10,
+        capitalSelection: 'Initial Capital'
       }
     }
   },
-  computed: {
-    resultValue: function () {
-      var result = (this.form.principal * (1 + this.form.rate) ** this.form.term)
+  methods: {
+    CalculateResult: function () {
+      if (this.form.capitalSelection === 'Initial Capital') {
+        return (this.form.principal * (1 + this.form.rate) ** this.form.term)
+      } else {
+        return (this.form.principal) / ((1 + this.form.rate) ** this.form.term)
+      }
+    },
+    RoundResult: function (result) {
       if (this.roundingSelection === 'twoDecimal') {
         result = result.toFixed(2)
       }
       return result
+    }
+  },
+  computed: {
+    resultValue: function () {
+      var result = this.CalculateResult()
+      result = this.RoundResult(result)
+      return result
+    },
+    CapitalSelection: {
+      get: function () {
+        return this.form.capitalSelection
+      },
+      set: function (newValue) {
+        this.form.capitalSelection = newValue
+      }
+    },
+    ResultLabel: function () {
+      if (this.CapitalSelection === 'Initial Capital') {
+        return 'Return value after ' + this.form.term + ' years:'
+      } else {
+        return 'Present value of this bond:'
+      }
     }
   }
 }
